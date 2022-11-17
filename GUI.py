f1e2c4908c7947bec9d56148174895cf3b9cad07
Ahmed Hesham <67082298+Ahmed122000@users.noreply.github.com>
2022-11-17 17:23:26 +0200

    finishing GUI

--- GUI.py
+++ GUI.py
@@ -1,16 +1,20 @@
+from ab.alpha_beta import alphabeta
+from minimax.MiniMax import MiniMax
+from state.State import State
 import numpy as np
 import pygame
 import sys
 
-COLOR_BLUE = (0, 0, 255)
-COLOR_BLACK = (0, 0, 0)
-COLOR_RED = (255, 0, 0)
-COLOR_YELLOW = (255, 255, 0)
+# constant values in the code
+COLOR_BLUE = (0, 0, 255)  # blue color RGB values
+COLOR_BLACK = (0, 0, 0)  # black color RGB values
+COLOR_RED = (255, 0, 0)  # red color RGB values
+COLOR_YELLOW = (255, 255, 0)  # yellow color RGB values
 
 
 # create initial board
 def create_board():
-    return np.zeros((6, 7))
+    return np.zeros((6, 7))  # start the game with empty board
 
 
 # drop the piece in the board according to the user selection
@@ -20,105 +24,108 @@ def drop_piece(board, row, col, piece):
 
 # check if the move the user chose is valid
 def check_valid_move(board, column):
-    if board[0, column] != 0:
+    if board[0, column] != 0:  # check the first row in the board if not 0 then its not empty to drop the piece
         return False
     return True
 
 
 # get the right row in the chosen column
 def get_next_open_row(board, col):
-    for i in range(5, -1, -1):
+    for i in range(5, -1, -1):  # go through all rows in the column col to find the current empty slot
         if board[i][col] == 0:
             return i
 
 
-# check if the last move is a finish move for the game
-def check_winning_move(board, piece):
-
-    for i in range(6):
-        for j in range(7):
-            if j <= 3:
-                if board[i][j] == board[i][j + 1] == board[i][j + 2] == board[i][j + 3] == piece:
-                    return True
-            if i < 3:
-                if board[i][j] == board[i + 1][j] == board[i + 2][j] == board[i + 3][j] == piece:
-                    return True
-            if i < 3 and j <= 3:
-                if board[i][j] == board[i + 1][j + 1] == board[i + 2][j + 2] == board[i + 3][j + 3] == piece:
-                    return True
-            if i >= 3 and 3 >= j:
-                if board[i][j] == board[i - 1][j + 1] == board[i - 2][j + 2] == board[i - 3][j + 3] == piece:
-                    return True
-    return False
-
-
-def draw_board(board):
+# draw the board in GUI
+def draw_board(screen, board):
     for c in range(7):
         for r in range(6):
-            pygame.draw.rect(screen, COLOR_BLUE, (c*100, r*100 + 100, 100, 100))
+            pygame.draw.rect(screen, COLOR_BLUE, (c * 100, r * 100 + 100, 100, 100))  # the board
             if board[r][c] == 0:
-                pygame.draw.circle(screen, COLOR_BLACK, (c*100+50, r*100+150), 45)
+                pygame.draw.circle(screen, COLOR_BLACK, (c * 100 + 50, r * 100 + 150), 45)  # empty slto is black circle
             elif board[r][c] == 1:
-                pygame.draw.circle(screen, COLOR_YELLOW, (c*100+50, r*100+150), 45)
+                pygame.draw.circle(screen, COLOR_YELLOW, (c * 100 + 50, r * 100 + 150),
+                                   45)  # player 1 plays yellow pieces
             else:
-                pygame.draw.circle(screen, COLOR_RED, (c*100+50, r*100+150), 45)
+                pygame.draw.circle(screen, COLOR_RED, (c * 100 + 50, r * 100 + 150), 45)  # player 2 plays red pieces
 
-    pygame.display.update()
+    pygame.display.update()  # display the board after checking the last status
 
 
-board = create_board()
-game_over = False
-turn = 0
+def check_end_game(board):
+    for i in range(7):
+        if board[0, i] == 0:
+            return False
+    return True
 
-pygame.init()
-width = height = 700
 
-screen = pygame.display.set_mode((width, height))
-draw_board(board)
-pygame.display.update()
-message = pygame.font.SysFont('monospace', 75)
+# start the game
+def play_game(algorithm, depth):
+    current_state = State(6485768453102907528)
+    board = create_board()  # create the board
+    game_over = False
+    turn = 0
+    algo = MiniMax(depth)
+    if algorithm == 1:
+        algo = alphabeta(depth)
 
-while not game_over:
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            sys.exit(0)
+    pygame.init()
+    width = height = 700
+    screen = pygame.display.set_mode((width, height))
+    draw_board(screen, board)
 
-        if e.type == pygame.MOUSEMOTION:
-            pygame.draw.rect(screen, COLOR_BLACK, (0, 0, width, 100))
-            pos = e.pos[0]
-            if turn == 0:
-                pygame.draw.circle(screen, COLOR_YELLOW, (pos, 50), 45)
-            else:
-                pygame.draw.circle(screen, COLOR_RED, (pos, 50), 45)
-        pygame.display.update()
+    message = pygame.font.SysFont('monospace', 75)  # display the winning message at the end of the game
 
-        if e.type == pygame.MOUSEBUTTONDOWN:
-            if turn == 0:
-                selected_column = e.pos[0] // 100
-                if check_valid_move(board, selected_column):
-                    row = get_next_open_row(board, selected_column)
-                    drop_piece(board, row, selected_column, 1)
-                if check_winning_move(board, 1):
-                    pygame.draw.rect(screen, COLOR_BLACK, (0, 0, width, 100))
-                    win_message = message.render("player 1 wins", True, COLOR_YELLOW)
-                    screen.blit(win_message, (40, 10))
-                    game_over = True
+    while not game_over:
 
-            elif turn == 1:
-                print("player 2 turn")
-                selected_column = e.pos[0] // 100
-                if check_valid_move(board, selected_column):
-                    row = get_next_open_row(board, selected_column)
-                    drop_piece(board, row, selected_column, 2)
-                if check_winning_move(board, 2):
-                    pygame.draw.rect(screen, COLOR_BLACK, (0, 0, width, 100))
-                    win_message = message.render("player 2 wins", True, COLOR_RED)
-                    screen.blit(win_message, (40, 10))
-                    game_over = True
+        if turn == 1:
+            pygame.draw.rect(screen, COLOR_BLACK, (0, 0, width, 100))  # clear an empty area before each step
+            pygame.display.update()
 
-            draw_board(board)
-            turn = (turn + 1) % 2
+            col = algo.get_next_state(current_state)
+            next_state = current_state.update_state(col, State.computer)
+            current_state = next_state
+            if check_valid_move(board, 7 - col):
+                row = get_next_open_row(board, 7 - col)
+                drop_piece(board, row, 7 - col, 2)
+                draw_board(screen, board)
+                turn = (turn + 1) % 2
+                draw_board(screen, board)
 
-            if game_over:
-                pygame.time.wait(3000)
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:  # quit the game if we pressed exit button
+                pygame.display.quit()
+                pygame.quit()  # end the program
+                game_over = True
+                break;
 
+            # let the pieces move with the mouse
+            if e.type == pygame.MOUSEMOTION:
+                pygame.draw.rect(screen, COLOR_BLACK, (0, 0, width, 100))  # clear an empty area before each step
+                pos = e.pos[0]  # get the x axis of the mouse
+                if turn == 0:
+                    pygame.draw.circle(screen, COLOR_YELLOW, (pos, 50), 45)  # player 1 plays with yellow pieces
+                pygame.display.update()
+
+            elif e.type == pygame.MOUSEBUTTONDOWN:
+                print("this is user turn")
+                if turn == 0:
+                    col = e.pos[0] // 100
+                    next_s = current_state.update_state(7-col, State.human)
+                    current_state = next_s
+                    if check_valid_move(board, col):
+                        row = get_next_open_row(board, col)
+                        drop_piece(board, row, col, 1)
+                        turn = (turn + 1) % 2
+
+                draw_board(screen, board)
+                game_over = check_end_game(board)
+                if game_over:
+                    keep_running = True
+                    while keep_running:
+                        for e in pygame.event.get():
+                            if e.type == pygame.QUIT:  # quit the game if we pressed exit button
+                                pygame.display.quit()
+                                pygame.quit()  # end the program
+                                keep_running = False
+                                break
--- main.py
+++ main.py
@@ -1,30 +1,61 @@
-from minimax.MiniMax import MiniMax
-from state.State import State
-from ab.alpha_beta import alphabeta
+from tkinter import *
+import tkinter as tk
+from GUI import play_game
+
+
+class Game(tk.Tk):
+    depth = 5  # save user input
+    algorithms = ['MiniMax without pruning', 'MiniMax with pruning']
+
+    def __init__(self):  # create main page
+        super().__init__()
+
+        self.x = IntVar()
+        self.config(background='#FEEBA0')  # main page background color
+        self.geometry("440x500")  # size of the page
+        self.resizable(False, False)  # set the page to fixed size
+        self.title('Connect 4 game')  # page title
+
+        self.frame = Frame(self)  # set a frame to hold input components
+        self.entry = Entry(self.frame,  # make entry field to read input from user
+                           font=('Arial', 25))
+        self.enter_button = Button(self.frame,  # make Enter button to enter the input
+                                   text="Enter",
+                                   command=self.get_depth,
+                                   font=("Arial", 20),
+                                   bg='lightblue'
+                                   )
+        self.entry.pack(side=LEFT, expand=True)  # add the entry to the frame and fill the window
+        self.enter_button.pack(side=RIGHT)  # put the button to the frame to the right of the entry
+        self.frame.pack()  # add the frame to the window
+
+        for index in range(len(self.algorithms)):  # create radio button for each algorithm to choose from them
+            self.radio_button = Radiobutton(self,
+                                            text=self.algorithms[index],
+                                            variable=self.x,  # variable to hold the choose
+                                            value=index,  # the value of each choose is its index
+                                            padx=0,
+                                            bg='#F5631A',
+                                            width=50,
+                                            indicatoron=0,
+                                            font=('Arial', 25))
+            self.radio_button.pack(anchor=W)  # add the radio buttons to window and set them to the west
+
+        self.play = Button(self,  # add button to start playing the game
+                           text='Play',
+                           command=self.play,
+                           font=("Comic Sans", 20),
+                           )
+        self.play.pack(pady=20)  # add the button to the window
+
+    def get_depth(self):
+        self.depth = int(self.entry.get())   # get the depth from the user
+
+    def play(self):
+        play_game(self.x.get(), self.depth)  # get the board and start the game
+
 
 if __name__ == '__main__':
-    current_state = State(6485768453102907528)
-    first_state = current_state
-    player = False
-    alg = MiniMax(4)
-    # alg = alphabeta(7)
 
-    while True:
-
-        current_state.print_state()
-        if current_state.is_terminal():
-            break
-
-        if player:
-            player = False
-            col = alg.get_next_state(current_state)
-            next_state = current_state.update_state(col, State.computer)
-            current_state = next_state
-        else:
-            player = True
-            col = int(input("Enter column number: "))
-            next_s = current_state.update_state(col, State.human)
-            current_state = next_s
-
-        # current_state.calculate_heuristic(0, True)
-        # current_state.calculate_heuristic(1, True)
+    game = Game()
+    game.mainloop() # start the game
